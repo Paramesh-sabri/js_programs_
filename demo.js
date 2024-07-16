342b332bfd282de21474e58bc2772250269d854f
PARAMESWARAN A B <160161902+Paramesh-sabri@users.noreply.github.com>
2024-07-16 16:58:29 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,90 @@
+// //1.CREATE AN ARRAY AND INITIALIZE IT
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// console.log("first element in the array is : ",arr[0]);
+
+
+
+
+// //2.ARRAY MANIPULATION
+// let arr=[1,2,3,4,5];
+// //push
+// arr.push(6);
+// console.log(arr);
+// //pop
+// arr.pop(6);
+// console.log(arr);
+// //shift
+// arr.shift(1);
+// console.log(arr);
+// //unshift
+// arr.unshift(1);
+// console.log(arr);
+
+
+
+
+// //3.ARRAY SEARCHING
+// let arr=[1,2,3,4,5,6,7,1,2,3,4,5,6,7];
+// //indexof
+// console.log(arr.indexOf(5));
+// //lastindexof
+// console.log(arr.lastIndexOf(7));
+
+
+
+
+// //4.ARRAY FILTERING
+// let arr=[1,2,3,4,5,6];
+// let arr2=arr.filter((x)=>{return x>3});
+// console.log(arr2);
+
+
+
+// //5.ARRAY MAPPING
+// let arr=[1,2,3,4,5,6];
+// let arr2=arr.map((x)=>{return x*2});
+// console.log(arr2);
+
+
+
+// //6.ARRAY SORT AND REVERSE
+// let arr=[1,2,3,4,1,2,3,4];
+// console.log(arr.reverse());
+// console.log(arr.sort());
+
+
+
+
+
+// //7.ARRAY JOIN AND SPLIT 
+// let arr=["hello","world","how","are","you"];
+// arr1=arr.join(" ");
+// console.log(arr1);
+// console.log(arr1.split(" "));
+
+
+
+// //8.ARRAY DESTRUCTION
+// let arr=[1,2,3,4,5,6,7,8,9];
+// let [a,b,c,d,e,f,g,h,i]=arr;
+// console.log(a,b,c,d,e,f,g,h,i);
+
+
+// //9.ARRAY SPREADING:
+// let arr=[1,2,3,4,5,6,7,8,9];
+// let [a,b,c,d,e,f,g,h,i,...rest]=arr;
+// console.log(a,b,c,d,e,f,g,h,i,rest);
+
+
+
+// // 10.MAX AND MIN NUMBER IN ARRAY 
+// let arr=[1,2,3,4,5,6,7,8,9];
+// let max=Math.max(...arr);
+// let min=Math.min(...arr);
+// console.log(max,min);
+
+
+
+
+
